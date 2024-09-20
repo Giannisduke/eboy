4b15c1b77bb2e0513dd791b1e83ebbadb81ecc2a
Giannisduke <admin@eboy.gr>
2024-09-20 18:50:38 +0300

    Deploy - 0001

--- site/web/app/themes/eboy0001/resources/scripts/router/index.js
+++ site/web/app/themes/eboy0001/resources/scripts/router/index.js
@@ -1,9 +1,10 @@
 import {createWebHistory, createRouter} from "vue-router";
+import Welcome from "../../views/partials/welcome.vue";
 import UserLocation from "../../views/partials/UserLocation.vue";
 import StoreList from "../../views/partials/StoreList.vue";
 const routes = [{
     path: "/",
-    component: UserLocation,
+    component: Welcome,
 },
 {
     path: "/shop/",
---
+++ site/web/app/themes/eboy0001/resources/views/partials/welcome.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="welcome">
+        <h1>Welcome</h1>
+    </section>
+</template>
+
